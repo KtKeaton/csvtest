5efd54cabcadaddf5dc09d42bc45e10600855c58
KtKeaton <ktr01@protonmail.com>
2022-02-27 22:05:21 +0800

    Controller

--- app/controllers/foos_controller.rb
+++ app/controllers/foos_controller.rb
@@ -1,6 +1,50 @@
 class FoosController < ApplicationController
+  before_action :find_foo, only: [:show, :edit, :update, :destroy]
 
-  def  index
-  
+  def index
+    @foos = Foo.all
+  end
+
+  def new
+    @foo = Foo.new  
+  end
+
+  def create
+    @foo = Foo.new(foo_params)
+    if @foo.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @foo = find_foo
+  end
+
+  def edit
+    @foo = find_foo
+  end
+
+  def update
+    @foo = find_foo
+    @foo.update(foo_params)
+    redirect_to foo_path(@foo)
+  end
+
+  def destroy
+    @foo = find_foo
+    @foo.destroy
+      redirect_to tasks_url, :notice => I18n.t(:task_close)
+    end
+  end
+
+private
+  def find_foo
+    @foo = Foo.find(params[:id])
+  end
+
+  def foo_params
+    params.require(:foo).permit(:name)
   end
 end
